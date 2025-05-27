25fe5646d78b8f4aa22276858ca4c58ec3d0c4a0
zjx0101 <63288514+zjx0101@users.noreply.github.com>
2025-05-27 17:22:29 +0800

    Update jquery.twentytwenty.js

--- projects/ObjectClear/assets/js/jquery.twentytwenty.js
+++ projects/ObjectClear/assets/js/jquery.twentytwenty.js
@@ -45,7 +45,7 @@
         let relativeX = e.pageX - offset.left;
       
         let clippedWidth = width * sliderPct;
-        let triggerZone = clippedWidth * 0.85;
+        let triggerZone = clippedWidth * 0.8;
       
 
         if (relativeX < triggerZone && relativeX >= 0) {
